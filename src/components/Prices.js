d9c783d413a0b34ee131a09cb0f94167d8eb7e66
kalwalt <info@kalwaltart.it>
2022-11-09 15:17:23 +0100

    new prices pdf

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import {FormattedMessage} from 'react-intl'
+import * as React from 'react'
+import { FormattedMessage } from 'react-intl'
+import Image from './PreviewCompatibleImage'
 import PropTypes from 'prop-types'
 
-const Prices = ({prices}) => {
+const Prices = ({ prices, imageInfo }) => {
   return (
-    <div className="section">
-      <nav className="level">
+    <div className="section ">
+      <a className="link is-info" href={prices} target="_blank" rel="noreferrer" id="prices">
+        <Image imageInfo={imageInfo} />
+      </a>
+      <nav className="level py-1" id="pdf-prices">
         <div className="level-item">
           <p className="title has-text-centered">
-            <FormattedMessage id="download"/>
-            <a className="link is-info" href={prices} target="_blank" rel="noreferrer" id="prices">       
-              <FormattedMessage id="rooms-prices"/>
+            <FormattedMessage id="download" />
+            <a className="link is-info" href={prices} target="_blank" rel="noreferrer" id="prices">
+              <FormattedMessage id="rooms-prices" />
             </a>
           </p>
         </div>
@@ -21,6 +25,7 @@ const Prices = ({prices}) => {
 
 Prices.propTypes = {
   prices: PropTypes.string,
+  imageInfo: PropTypes.object
 };
 
 export default Prices;
