2082d3cebfcd0da65c7fc27c462cb9581ea9c17c
kalwalt <github@kalwaltart.it>
2025-01-22 14:56:24 +0100

    testing new CIN component

---
+++ src/components/CIN.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FormattedMessage } from 'react-intl';
+
+const CIN = () => {
+    <section className="section">
+      <br/>
+      <div className="container content">
+        <div className="container is-fluid has-text-light">
+           <FormattedMessage id="cin"/>
+        </div>
+      </div>
+    </section>
+}
+
+export default CIN
